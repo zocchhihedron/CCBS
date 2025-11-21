eba423e56dab604902083e7df0185e10d9191797
zocchhihedron <96637745+zocchhihedron@users.noreply.github.com>
2025-11-21 17:57:08 +0100

    WIP

--- sequence_encoding.py
+++ sequence_encoding.py
@@ -1,7 +1,17 @@
+'''A sandbox for encoding abstractions for sequences used as input in the BCPNN network over 3 levels of 
+hierarchy: individual hypercolumns, individual patterns/states and a whole sequence of patterns over time.'''
+
 import numpy as np
 
-def abs1(minicolumns):
-    '''Creates a one-hot-encoded vectors each representing a hypercolumn with zeros 
+def abs1(minicolumns, i=int):
+    '''Creates a one-hot-encoded vector representing a hypercolumn with zeros 
     at every position except for the index of the unit which is activated'''
+    hypercolumn_act = np.zeros(minicolumns)
+    hypercolumn_act[i] = 1
+    return hypercolumn_act
 
-def abs2(patterns, )
+def abs2(hypercolumns):
+    return
+
+def abs3(patterns):
+    return
