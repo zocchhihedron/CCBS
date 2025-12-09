da7d5bc08926f2463af49d1cf52ef0c8025543f6
zocchhihedron <96637745+zocchhihedron@users.noreply.github.com>
2025-12-09 17:32:52 +0100

    Partially fixed current & WTA output

--- BCPNN.py
+++ BCPNN.py
@@ -4,7 +4,7 @@ import matplotlib as plt
 
 class BCPNN:
     def __init__(self, hypercolumns, minicolumns, g_beta = 1.0, beta = 1.0, tau_m = 0.02, 
-                 g_I = 1.0, g_a = 97.0, tau_p = 10.0, tau_z_pre = 0.15, tau_z_post = 0.005, tau_a=2.70):
+                 g_I = 1.0, g_a = 1.0, tau_p = 10.0, tau_z_pre = 0.15, tau_z_post = 0.005, tau_a=2.70):
         '''A simplified model of the BCPNN network with defined network variables and functions for creating
         sequences, training the network, updating variables and running the network for given sequences.
         A helper softmax function for the updating function is included. Default values for instance variables 
@@ -34,9 +34,12 @@ class BCPNN:
 
         # Weights and probabilities
         self.w = np.zeros((self.n_units, self.n_units))
-        self.p_pre = np.ones(self.n_units) * (1.0 / self.minicolumns)
-        self.p_post = np.ones(self.n_units) * (1.0 / self.minicolumns)
-        self.p_co = np.ones((self.n_units, self.n_units)) * 1.0 / (self.minicolumns ** 2)
+        self.p_pre  = np.ones(self.n_units) / self.n_units
+        self.p_post = np.ones(self.n_units) / self.n_units
+        self.p_co   = np.ones((self.n_units,self.n_units)) / (self.n_units**2)
+        #self.p_pre = np.ones(self.n_units) * (1.0 / self.minicolumns)
+        #self.p_post = np.ones(self.n_units) * (1.0 / self.minicolumns)
+        #self.p_co = np.ones((self.n_units, self.n_units)) * 1.0 / (self.minicolumns ** 2)
         self.beta = np.zeros(self.n_units) # Compatible shape for calculation of s, change afterwards ASAP to correct interpretation
         #self.beta = np.log(np.ones_like(self.o) * (1.0 / self.minicolumns))
 
@@ -53,13 +56,15 @@ class BCPNN:
                                         - self.g_a * self.a  # Adaptation
                                         + noise  # This last term is the noise
                                         - self.s)  # s follow all of the s above  
+        print('s after state update: ',self.s)
         self.s_history.append(self.s.copy())
 
         # WTA mechanism
         argmax = np.argmax(self.s)
+        self.o = np.zeros(self.n_units) # Reset
         self.o[argmax] = 1.0
         self.o_history.append(self.o.copy())
-        print(self.o)
+        print('o after WTA: ', self.o)
 
 
         # Update the adaptation
@@ -72,36 +77,34 @@ class BCPNN:
         # Probabilities
         self.p_pre += (dt / self.tau_p) * (self.z_pre - self.p_pre)
         self.p_post += (dt / self.tau_p) * (self.z_post - self.p_post)
-        self.p_co += (dt / self.tau_p) * (np.outer(self.z_pre, self.z_post))
+        self.p_co += (dt / self.tau_p) * (np.outer(self.z_pre, self.z_post) - self.p_co)
 
     def update_weights(self, dt = 5.0, I = None, noise = 0.0):
         '''Updates weights and bias for training.'''
-        print('beta: ', self.beta)
+        #print('beta: ', self.beta)
 
         # Weights  
         eps = 1e-9 # Prevent log(0) as output
-        self.w = np.log((self.p_co + eps) / (np.outer(self.p_pre, self.p_post)))
+        self.w = np.log((self.p_co + eps) / (np.outer(self.p_pre, self.p_post) + eps))
 
         # Bias
-        # self.beta = np.log(self.p_post) FIX!
+        self.beta = np.log(self.p_post + eps) 
 
     def produce_sequences(self, n_patterns = None, s=0, r=0):
         pass
 
-    def train(self, I, pattern_dur, epochs, dt = 1.0):
+    def train(self, I, pattern_dur, epochs, dt = 0.01):
         '''Trains the network with a sequence as external input.'''
         for epoch in range(epochs): 
             for pattern in seq: # Choose a pattern / time state
                     # One-hot encode the pattern here before updating to ensure the shape matches s and o
                     # Modularize later on
-                    encoded_pattern = []
-                    for hypercolumn in pattern:
-                        hyp_onehot = np.zeros(minicolumns) # one-hot encoding of a single hypercolumn
-                        hyp_onehot[hypercolumn] = 1 # the active unit
-                        encoded_pattern.append(hyp_onehot) # append each encoding to the encoding of the whole pattern
-                        print(encoded_pattern)
-                    self.update_state(I = encoded_pattern)
-                    self.update_weights()
+                    encoded_pattern = np.zeros(self.n_units)
+                    for h, mc in enumerate(pattern):
+                        index = h * self.minicolumns + mc
+                        encoded_pattern[index] = 1
+                    self.update_state(I = encoded_pattern, dt = 0.01, noise = 0.0)
+                    self.update_weights(dt = 0.01, I = None, noise = 0.0)
 
     def recall(self):
         '''Sequence recall given a cue.'''
@@ -114,5 +117,5 @@ if __name__ == '__main__':
     nn = BCPNN(hypercolumns, minicolumns)
     nn.train(I = seq, pattern_dur = 1, epochs = 10)
 
-    #plt.plot(nn.s_history_history,nn.o_history)
-    #plt.show
+    plt.plot(nn.s_history, nn.o_history)
+    plt.show
