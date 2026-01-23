5be8265670fef13a6999e497ae071cef98dd31e6
zocchhihedron <96637745+zocchhihedron@users.noreply.github.com>
2026-01-23 13:57:15 +0100

    Update

--- functions.py
+++ functions.py
@@ -30,6 +30,7 @@ def update_weights(nn, dt, noise = 0):
     '''Updates weights and biases per time unit for network training.'''
 
     # Weights  
+
     eps = 1e-9 # Prevent log(0) as output
     nn.w = np.log((nn.p_co + eps) / (np.outer(nn.p_pre, nn.p_post) + eps))
 
@@ -38,6 +39,7 @@ def update_weights(nn, dt, noise = 0):
 
 def strict_max(x, minicolumns):
     '''Reshapes the current vector into the unit activation vector.'''
+
     x = np.reshape(x, (x.size // minicolumns, minicolumns))
     z = np.zeros_like(x)
     maxes = np.argmax(x, axis=1)
@@ -46,55 +48,64 @@ def strict_max(x, minicolumns):
 
     return z.reshape(x.size)
 
-def train_pattern(nn, dt, g_I, Ndt, I, I_amp, learning = True, save_history = True):
+def train_pattern(nn, dt, g_I, Ndt, I, learning = True, save_history = True):
     '''Trains the network on a pattern.'''
-    if I.all() == None:
-        I = np.zeros(nn.hypercolumns*nn.minicolumns)
+
+    if I is None:
+        I = np.zeros(nn.n_units)
     for i in range(Ndt):
         update_state(nn, dt = dt, I = I, g_I = g_I, noise = 0)
         if learning:
             update_weights(nn, noise = 0, dt = dt)
         if save_history:
-            nn.s_history.append(nn.s)
-            nn.o_history.append(nn.o)
+            nn.o_history.append(nn.o.copy())
+            nn.s_history.append(nn.s.copy())
             if learning:
                 nn.w0_history.append(nn.w[0])
 
-def train_sequence(nn, dt, Ndt, seq, I_amp, g_I, learning = True, save_history = True):
+def train_sequence(nn, dt, Ndt, seq, g_I, learning = True, save_history = True):
     '''Trains the network on a sequence of patterns.'''
+
     for pattern in seq:
-        train_pattern(nn, dt = dt, Ndt = Ndt, I = pattern, I_amp = I_amp, g_I = g_I, learning = True, save_history = True)
+        train_pattern(nn, dt = dt, Ndt = Ndt, I = pattern, g_I = g_I, learning = True, save_history = True)
 
 def recall(nn, dt, g_I, I_cue, no_patterns, cue_steps, recall_steps):
     '''Recalls a sequence learned by the network by updating the network state without updating weights and biases.'''
+
     nn.o_history = []
-    # Cueing with first element of the input
-    I_zero = np.zeros(nn.n_units)
-    for _ in range(no_patterns):
-        for _ in range(cue_steps):
-            update_state(nn=nn, I=I_cue, dt=dt, g_I=g_I)
-        # The recalled patterns are saved with each step of recall
-        nn.o_history.append(nn.o)
+
+    # Cueing 
+    for _ in range(cue_steps):
+        update_state(nn, I=I_cue, dt = dt, g_I = g_I)
+        
+    # Free recall
+    for _ in range(recall_steps):
+        update_state(nn, dt = dt, I = np.zeros(nn.n_units), g_I = g_I)
+        nn.o_history.append(nn.o.copy())
+
     # The history of the unit activations is the recalled sequence
     print(nn.o_history)
 
 def one_hot_encode(pattern, hypercolumns, minicolumns):
     '''Reshapes an indexed pattern representation into a one-hot encoded
     hypercolumn representation'''
+
     x = np.zeros(hypercolumns * minicolumns)
     for hyp, minic in enumerate(pattern):
         x[hyp * minicolumns + minic] = 1
+
     return x
 
 
 if __name__ == '__main__':
+
     dt = 0.01
     hypercolumns, minicolumns = 2, 3
     nn = BCPNN(hypercolumns, minicolumns)
     seq = np.array([one_hot_encode([0,1], hypercolumns, minicolumns), one_hot_encode([1,1], hypercolumns, minicolumns)])
     no_patterns = seq.shape[0]
-    train_sequence(nn = nn, dt = dt, Ndt = 100, seq = seq, I_amp = nn.g_I, g_I = nn.g_I, learning = True, save_history = True)
-    recall(nn, I_cue = np.zeros(15), dt = dt, g_I = nn.g_I, cue_steps = 5, no_patterns = no_patterns, recall_steps = 10)
+    train_sequence(nn = nn, dt = dt, Ndt = 100, seq = seq, g_I = nn.g_I, learning = True, save_history = True)
+    recall(nn, I_cue = np.zeros(nn.n_units), dt = dt, g_I = nn.g_I, cue_steps = 5, no_patterns = no_patterns, recall_steps = 10)
 
     # Print certain state variables & weights
 
